700af1da9d3b6a6bffff054ae98eff7c00c1e4a7
lequysang <lequysang84@gmail.com>
2013-03-01 19:48:54 +0700

    Add complete_Json

---
+++ DragCollectionViewCell/DragCollectionViewCell/MyFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  MyFlowLayout.h
+//  DragCollectionViewCell
+//
+//  Created by Sang Quý Lê on 2/26/13.
+//  Copyright (c) 2013 Quy Sang Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyFlowLayout : UICollectionViewFlowLayout
+
+@end
